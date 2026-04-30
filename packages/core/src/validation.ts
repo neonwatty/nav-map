741bc1a4eda388490dcc8c14f79a17f1e7bce8de
Jeremy Watt <jermwatt@gmail.com>
2026-04-30 15:34:42 -0700

    ci: archive scanner crawl diagnostics (#78)

---
+++ packages/core/src/validation.ts
@@ -0,0 +1,21 @@
+export { validateGraph } from './utils/validateGraph';
+export type { GraphValidationResult, GraphValidationError } from './utils/validateGraph';
+export type {
+  NavMapGraph,
+  NavMapNode,
+  NavMapEdge,
+  NavMapGroup,
+  NavMapSharedNav,
+  NavMapAnalytics,
+  GroupColors,
+  GroupColorMap,
+  NavMapFlow,
+  NavMapFlowStep,
+  NavMapFlowGallery,
+  ViewMode,
+  EdgeMode,
+  NavMapTheme,
+  CoverageData,
+  CoverageTestRef,
+  TestStatus,
+} from './types';
